d14cd3ab9e40a835889aea4a5d65d90ce030de64
Ramesh Babu Prudhvi <rprudhvi@outlook.com>
2021-08-16 13:33:50 +0530

    Bump Selcukes version

--- src/main/java/io/github/selcukes/example/SuiteListener.java
+++ src/main/java/io/github/selcukes/example/SuiteListener.java
@@ -5,23 +5,20 @@ import lombok.SneakyThrows;
 import org.testng.ISuite;
 import org.testng.ISuiteListener;
 
-import java.awt.*;
-import java.io.File;
-
 @CustomLog
 public class SuiteListener implements ISuiteListener {
     public void onStart(ISuite suite) {
         logger.info(() -> "Test Suite execution started...");
+        System.setProperty("cucumber.publish.quiet", "true");
         String reportsPath = "target/cucumber-reports";
 
-        String plugin = "pretty, html:" + reportsPath + "/cucumber.html, json:" + reportsPath + "/cucumber.json, " +
-            "io.github.selcukes.extent.report.ExtentCucumberAdapter:";
+        String plugin = "html:" + reportsPath + "/cucumber.html, json:" + reportsPath + "/cucumber.json, " +
+            "io.github.selcukes.extent.report.SelcukesExtentAdapter:";
         System.setProperty("cucumber.plugin", plugin);
     }
 
     @SneakyThrows
     public void onFinish(ISuite suite) {
         logger.info(() -> "Test Suite execution completed...");
-        Desktop.getDesktop().open(new File("target/extent-reports/TestReport.html"));
     }
 }
