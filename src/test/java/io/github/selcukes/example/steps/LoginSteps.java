32f6cb84aa24b6ee78ee676aaf1e4ccdcd6d6d68
Ramesh Babu <rprudhvi@outlook.com>
2022-04-22 21:03:53 +0530

    Use core module changes

--- src/test/java/io/github/selcukes/example/steps/LoginSteps.java
+++ src/test/java/io/github/selcukes/example/steps/LoginSteps.java
@@ -4,9 +4,10 @@ package io.github.selcukes.example.steps;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import io.github.selcukes.example.utils.DriverManager;
-import io.github.selcukes.extent.report.Reporter;
+import io.github.selcukes.core.listener.EventCapture;
 import io.github.selcukes.example.pages.LoginPage;
+import io.github.selcukes.example.utils.TestDriverManager;
+import io.github.selcukes.extent.report.Reporter;
 
 
 public class LoginSteps {
@@ -14,16 +15,16 @@ public class LoginSteps {
 
     LoginPage loginPage;
 
-    public LoginSteps(DriverManager driverManager) {
+    public LoginSteps(TestDriverManager driverManager) {
         loginPage = new LoginPage(driverManager.getDriver());
     }
 
     @Given("{} is on Home Page")
     public void userIsOnHomePage(String user) {
-
+        EventCapture.FIELD_ATTRIBUTE = "title";
+        loginPage.enableDriverEvents();
         loginPage.open("http://www.google.com/");
         Reporter.getReport().attachScreenshot();
-
     }
 
 
@@ -35,9 +36,9 @@ public class LoginSteps {
     @And("Switch to User{int} browser window")
     public void switchToWindow(int i) {
         if (i > 2)
-            loginPage.switchToWindow(i - 3);
+            loginPage.switchWindow(i - 3);
         else
-            loginPage.switchToWindow(i - 1);
+            loginPage.switchWindow(i - 1);
         loginPage.enter("User" + i);
     }
 
@@ -45,15 +46,17 @@ public class LoginSteps {
     public void userEnters(String text) {
         loginPage.enter(text);
     }
+
     @When("User clicks on {string} edit Icon")
     @When("User clicks on {string} icon")
     public void userClicks(String text) {
         System.out.println("Hello");
     }
+
     @When("User clicks on {string} action button")
     @When("User clicks on {string} button")
     public void userClicksButton(String text) {
-        System.out.println("Hello"+text);
+        System.out.println("Hello" + text);
     }
 }
 
