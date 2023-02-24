a7a62cc1410182dbc5d488c8a29f9fba584f50d8
Ramesh Babu <rprudhvi@outlook.com>
2023-02-24 11:39:58 +0530

    Ignore GraphQL Test

--- src/test/java/io/github/selcukes/example/cucumber/steps/CucumberHooks.java
+++ src/test/java/io/github/selcukes/example/cucumber/steps/CucumberHooks.java
@@ -15,57 +15,57 @@ import org.openqa.selenium.WebDriver;
 
 @CustomLog
 public class CucumberHooks {
-    public static ThreadLocal<String> testName = new InheritableThreadLocal<>();
-    WebDriver driver;
+	public static ThreadLocal<String> testName = new InheritableThreadLocal<>();
+	WebDriver driver;
 
-    public CucumberHooks(TestContext driverManager) {
-        driver = driverManager.getDriver();
-    }
+	public CucumberHooks(TestContext driverManager) {
+		driver = driverManager.getDriver();
+	}
 
-    @BeforeAll
-    public static void beforeAll() {
-        logger.info(() -> "Before All ...");
-    }
+	@BeforeAll
+	public static void beforeAll() {
+		logger.info(() -> "Before All ...");
+	}
 
-    @AfterAll
-    public static void afterAll() {
-        logger.info(() -> "After All ...");
-    }
+	@AfterAll
+	public static void afterAll() {
+		logger.info(() -> "After All ...");
+	}
 
-    @Before
-    public void beforeTest(Scenario scenario) {
-        String test = getFeatureName(scenario) + "::" + scenario.getName();
-        testName.set(test);
-        // Reporter.getReporter().initSnapshot(driver); //Initialise Full page
-        // screenshot
-        logger.info(() -> "Starting Scenario .." + scenario.getName());
-    }
+	@Before
+	public void beforeTest(Scenario scenario) {
+		String test = getFeatureName(scenario) + "::" + scenario.getName();
+		testName.set(test);
+		// Reporter.getReporter().initSnapshot(driver); //Initialise Full page
+		// screenshot
+		logger.info(() -> "Starting Scenario .." + scenario.getName());
+	}
 
-    @BeforeStep
-    public void beforeStep(Scenario scenario) {
-        logger.info(() -> "Before Step");
-    }
+	@BeforeStep
+	public void beforeStep(Scenario scenario) {
+		logger.info(() -> "Before Step");
+	}
 
-    @AfterStep
-    public void afterStep(Scenario scenario) {
-        logger.info(() -> "After Step");
-        try {
-            byte[] bytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(bytes, "image/png", "screenshot");
-        } catch (Exception ignored) {
-        }
+	@AfterStep
+	public void afterStep(Scenario scenario) {
+		logger.info(() -> "After Step");
+		try {
+			byte[] bytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(bytes, "image/png", "screenshot");
+		} catch (Exception ignored) {
+		}
 
-        // Reporter.attachScreenshot(); //Attach Full page screenshot
-    }
+		// Reporter.attachScreenshot(); //Attach Full page screenshot
+	}
 
-    @After
-    public void afterTest(Scenario scenario) {
-        logger.info(() -> "Completed Scenario .." + scenario.getName());
-    }
+	@After
+	public void afterTest(Scenario scenario) {
+		logger.info(() -> "Completed Scenario .." + scenario.getName());
+	}
 
-    public static String getFeatureName(Scenario scenario) {
-        String featureName = scenario.getUri().getPath();
-        featureName = featureName.substring(featureName.lastIndexOf("/") + 1, featureName.indexOf("."));
-        return featureName;
-    }
+	public static String getFeatureName(Scenario scenario) {
+		String featureName = scenario.getUri().getPath();
+		featureName = featureName.substring(featureName.lastIndexOf("/") + 1, featureName.indexOf("."));
+		return featureName;
+	}
 }
