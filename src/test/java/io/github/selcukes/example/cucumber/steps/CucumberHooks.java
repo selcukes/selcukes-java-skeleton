7fb81f3237ebe5e18e4a078141707154752b98be
Ramesh Babu <rprudhvi@outlook.com>
2023-02-26 15:07:05 +0530

    Revert Screenshot changes

--- src/test/java/io/github/selcukes/example/cucumber/steps/CucumberHooks.java
+++ src/test/java/io/github/selcukes/example/cucumber/steps/CucumberHooks.java
@@ -9,48 +9,55 @@ import io.cucumber.java.BeforeStep;
 import io.cucumber.java.Scenario;
 import io.github.selcukes.example.cucumber.utils.TestContext;
 import io.github.selcukes.excel.ScenarioContext;
-import io.github.selcukes.extent.report.Reporter;
 import lombok.CustomLog;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 @CustomLog
 public class CucumberHooks {
-	WebDriver driver;
+    WebDriver driver;
 
-	public CucumberHooks(TestContext driverManager) {
-		driver = driverManager.getDriver();
-	}
+    public CucumberHooks(TestContext driverManager) {
+        driver = driverManager.getDriver();
+    }
 
-	@BeforeAll
-	public static void beforeAll() {
-		logger.info(() -> "Before All ...");
-	}
+    @BeforeAll
+    public static void beforeAll() {
+        logger.info(() -> "Before All ...");
+    }
 
-	@AfterAll
-	public static void afterAll() {
-		logger.info(() -> "After All ...");
-	}
+    @AfterAll
+    public static void afterAll() {
+        logger.info(() -> "After All ...");
+    }
 
-	@Before
-	public void beforeTest(Scenario scenario) {
-		ScenarioContext.setTestName(scenario);
-		Reporter.getReporter().initSnapshot(driver);
-		logger.info(() -> "Starting Scenario .." + scenario.getName());
-	}
+    @Before
+    public void beforeTest(Scenario scenario) {
+        ScenarioContext.setTestName(scenario);
 
-	@BeforeStep
-	public void beforeStep(Scenario scenario) {
-		logger.info(() -> "Before Step");
-	}
+        logger.info(() -> "Starting Scenario .." + scenario.getName());
+    }
 
-	@AfterStep
-	public void afterStep(Scenario scenario) {
-		Reporter.getReporter().attachScreenshot();
-	}
+    @BeforeStep
+    public void beforeStep(Scenario scenario) {
+        logger.info(() -> "Before Step");
+    }
 
-	@After
-	public void afterTest(Scenario scenario) {
-		ScenarioContext.removeTestName();
-		logger.info(() -> "Completed Scenario .." + scenario.getName());
-	}
+    @AfterStep
+    public void afterStep(Scenario scenario) {
+        logger.info(() -> "After Step");
+
+        try {
+            byte[] bytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(bytes, "image/png", "screenshot");
+        } catch (Exception ignored) {
+        }
+    }
+
+    @After
+    public void afterTest(Scenario scenario) {
+        ScenarioContext.removeTestName();
+        logger.info(() -> "Completed Scenario .." + scenario.getName());
+    }
 }
