5ea9b81494b9256a7ac51480fae38ae5d99019e6
Ramesh Babu <rprudhvi@outlook.com>
2023-03-21 13:10:19 +0530

    Updated Test

--- src/test/java/io/github/selcukes/example/testng/api/GraphQLTest.java
+++ src/test/java/io/github/selcukes/example/testng/api/GraphQLTest.java
@@ -3,53 +3,53 @@ package io.github.selcukes.example.testng.api;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.github.selcukes.core.page.ApiPage;
 import io.github.selcukes.core.page.Pages;
-import java.util.UUID;
 import org.testng.annotations.Test;
 
+import java.util.UUID;
+
 public class GraphQLTest {
-	ApiPage page;
+    private static final String API_URL = "https://api.spacex.land/graphql/";
+    ApiPage page;
 
-	@Test(enabled = false, description = "Get Company data and verify CEO")
-	public void verifyCeoTest() {
+    @Test(enabled = false, description = "Get Company data and verify CEO")
+    public void verifyCeoTest() {
+        var query = new GraphQLQuery();
+        query.setQuery("{ company { name ceo coo } }");
+        page = Pages.apiPage();
+        JsonNode body = page.request(API_URL).post(query).bodyJson().at("/data/company/ceo");
+        page.assertThat().object(body.asText()).isEqualTo("Elon Musk");
+    }
 
-		GraphQLQuery query = new GraphQLQuery();
-		query.setQuery("{ company { name ceo coo } }");
-		page = Pages.apiPage();
-		JsonNode body = page.request("https://api.spacex.land/graphql/").post(query).bodyJson().at("/data/company/ceo");
-		page.assertThat().object(body.asText()).isEqualTo("Elon Musk");
-	}
+    @Test(enabled = false, description = "Get Launches and verify MissionName should be 'Thaicom6'")
+    public void verifyMissionName() {
+        var query = new GraphQLQuery();
+        query.setQuery("query getLaunches($limit: Int!){ launches(limit: $limit) { mission_name } }");
 
-	@Test(enabled = false, description = "Get Launches and verify MissionName should be 'Thaicom6'")
-	public void verifyMissionName() {
+        var queryLimit = new QueryLimit();
+        queryLimit.setLimit(10);
+        query.setVariables(queryLimit);
 
-		GraphQLQuery query = new GraphQLQuery();
-		query.setQuery("query getLaunches($limit: Int!){ launches(limit: $limit) { mission_name } }");
+        page = Pages.apiPage();
+        JsonNode body = page.request(API_URL).post(query).bodyJson().at("/data/launches")
+            .get(0).get("mission_name");
+        page.assertThat().object(body.asText()).isEqualTo("Thaicom 6");
+    }
 
-		QueryLimit queryLimit = new QueryLimit();
-		queryLimit.setLimit(10);
-		query.setVariables(queryLimit);
+    @Test(enabled = false, description = "Add User and verify returned data should correspond to data sent")
+    public void verifyUserData() {
 
-		page = Pages.apiPage();
-		JsonNode body = page.request("https://api.spacex.land/graphql/").post(query).bodyJson().at("/data/launches")
-				.get(0).get("mission_name");
-		page.assertThat().object(body.asText()).isEqualTo("Thaicom 6");
-	}
+        var query = new GraphQLQuery();
+        query.setQuery(
+            "mutation insert_users ($id: uuid!, $name: String!, $rocket: String!) { insert_users(objects: {id: $id, name: $name, rocket: $rocket}) { returning { id name rocket } } }");
 
-	@Test(enabled = false, description = "Add User and verify returned data should correspond to data sent")
-	public void verifyUserData() {
+        var myUser = User.builder().id(UUID.randomUUID()).name("Selcukes").rocket("One Stop Automation").build();
 
-		GraphQLQuery query = new GraphQLQuery();
-		query.setQuery(
-				"mutation insert_users ($id: uuid!, $name: String!, $rocket: String!) { insert_users(objects: {id: $id, name: $name, rocket: $rocket}) { returning { id name rocket } } }");
+        query.setVariables(myUser);
 
-		User myUser = User.builder().id(UUID.randomUUID()).name("Selcukes").rocket("One Stop Automation").build();
-
-		query.setVariables(myUser);
-
-		page = Pages.apiPage();
-		JsonNode body = page.request("https://api.spacex.land/graphql/").post(query).bodyJson()
-				.at("/data/insert_users/returning").get(0);
-		page.assertThat().object(body.get("name").asText()).isEqualTo("Selcukes");
-		page.assertThat().object(body.get("rocket").asText()).isEqualTo("One Stop Automation");
-	}
+        page = Pages.apiPage();
+        JsonNode body = page.request(API_URL).post(query).bodyJson()
+            .at("/data/insert_users/returning").get(0);
+        page.assertThat().object(body.get("name").asText()).isEqualTo("Selcukes");
+        page.assertThat().object(body.get("rocket").asText()).isEqualTo("One Stop Automation");
+    }
 }
